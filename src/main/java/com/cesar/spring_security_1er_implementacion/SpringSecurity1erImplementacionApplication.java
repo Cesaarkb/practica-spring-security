d9d5f4b8f4fbd9bd93c5b30025fe18b39fe89f22
Cesaarkb <cesaarkb@gmail.com>
2025-06-02 18:17:18 -0600

    primer commit de carga

---
+++ src/main/java/com/cesar/spring_security_1er_implementacion/SpringSecurity1erImplementacionApplication.java
@@ -0,0 +1,13 @@
+package com.cesar.spring_security_1er_implementacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurity1erImplementacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurity1erImplementacionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cesar/spring_security_1er_implementacion/SpringSecurity1erImplementacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesar.spring_security_1er_implementacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurity1erImplementacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
