3dd2c63270d0ab9282205dcd827c53ff5b6bb3a3
Cesaarkb <cesaarkb@gmail.com>
2025-06-03 18:04:35 -0600

    avances de seguridad

--- src/main/java/com/cesar/spring_security_1er_implementacion/security/UserDetailsServiceImpl.java
+++ src/main/java/com/cesar/spring_security_1er_implementacion/security/UserDetailsServiceImpl.java
@@ -1,4 +1,35 @@
 package com.cesar.spring_security_1er_implementacion.security;
 
-public class UserDetailsServiceImpl {
+import com.cesar.spring_security_1er_implementacion.model.User;
+import com.cesar.spring_security_1er_implementacion.repository.UserRepository;
+import com.cesar.spring_security_1er_implementacion.utils.SecurityUtils;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Set;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(()-> new UsernameNotFoundException("usuario no existe " + username));
+
+        Set<GrantedAuthority> authorities = Set.of(SecurityUtils.convertToAuthority(user.getRol().name()));
+
+        return UserDetailsImpl.builder()
+                .user(user)
+                .id(user.getId())
+                .username(username)
+                .password(user.getPassword())
+                .authorities(authorities)
+                .build();
+    }
 }
--- src/main/java/com/cesar/spring_security_1er_implementacion/service/UserServiceImpl.java
+++ src/main/java/com/cesar/spring_security_1er_implementacion/service/UserServiceImpl.java
@@ -20,7 +20,7 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public User createUser(User user){
-        passwordEncoder.encode(user.getPassword());
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 
